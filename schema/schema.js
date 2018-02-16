01d878e6e8530aa134dd4b2018da5c7ae4d30769
aneesdhansey <aneesdhansey@gmail.com>
2018-02-17 00:17:11 +0530

    Initial Commit

---
+++ schema/schema.js
@@ -0,0 +1,68 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList
+} = require('graphql');
+const axios = require('axios');
+
+const baseUrl = 'http://localhost:3000';
+
+
+const CompanyType = new GraphQLObjectType({
+    name : 'Company',
+    fields : () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        users : {
+            type : new GraphQLList(UserType),
+            resolve(parentValue, args){
+                return axios.get(`${baseUrl}/companies/${parentValue.id}/users`)
+                            .then(resp => resp.data);
+            }
+        }
+    })
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        company : {
+            type : CompanyType,
+            resolve(parentValue, args){
+                return axios.get(`${baseUrl}/companies/${parentValue.companyId}`)
+                .then(resp => resp.data);
+            }
+        }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`${baseUrl}/users/${args.id}`)
+                    .then(resp => resp.data);
+            }
+        },
+        company: {
+            type : CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args){
+                return axios.get(`${baseUrl}/companies/${args.id}`).then(resp => resp.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query : RootQuery
+});
